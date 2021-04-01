49bbb9568206dbdc8a66dd92d1dcd74eceb0d434
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-01 16:32:54 +0000

    categ routes/links setup

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,7 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     # @products = Product.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
