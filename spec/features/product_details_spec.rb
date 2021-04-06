5bd4af70d7c1e1d304a2a285e572b2fd720d1511
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-06 21:41:05 +0000

    test for product details page navigate

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "navigate from home -> product detail page" do
+    # ACT
+    visit '/'
+    first('article.product').click_on 'Details »'
+    
+   
+    
+
+    # VERIFY
+    expect(page).to have_content('Apparel')
+    # DEBUG
+    save_screenshot
+    # puts page.html
+  end
+
+end
