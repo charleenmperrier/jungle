79a9bf6c6c6fb8ed9a3298dd46ed4909142ad327
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-06 01:59:20 +0000

    password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "validates password & password_confirmation" do
+      user = User.new(
+        first_name: "first",
+        last_name: 'last',
+        email: 'email',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      user.save!
+
+      expect(user.password).to eq(user.password_confirmation)
+    end
+
+    it "password & password_confirmation must match" do
+      user = User.new(
+        first_name: "first",
+        last_name: 'last',
+        email: 'email',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      user.save!
+
+      expect(user.password).not_to be_empty
+      expect(user.password_confirmation).not_to be_empty
+    end
+
+    
+  end
 end
