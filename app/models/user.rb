0365964ffe4adccb018bff217a1b757d8f6555eb
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-06 03:02:07 +0000

    validates email and passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true, length: { minimum: 3 }
+  validates :email, :uniqueness => { :case_sensitive => false }, presence: true
   # validates :password_digest, presence: true
   
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
 
-    it "validates password & password_confirmation" do
+    it "validates password & password_confirmation && matching" do
       user = User.new(
         first_name: "first",
         last_name: 'last',
@@ -11,25 +11,86 @@ RSpec.describe User, type: :model do
         password: 'password',
         password_confirmation: 'password'
       )
-      user.save!
-
+      
+      user2 = User.new(
+        first_name: "first",
+        last_name: 'last',
+        email: 'email2',
+        password: 'password',
+        password_confirmation: ''
+      )
+      
       expect(user.password).to eq(user.password_confirmation)
+      expect(user2).to_not be_valid
     end
 
-    it "password & password_confirmation must match" do
-      user = User.new(
+    it "validate if email has been used before" do
+      user = User.create(
         first_name: "first",
         last_name: 'last',
-        email: 'email',
+        email: 'email@email.com',
         password: 'password',
         password_confirmation: 'password'
       )
-      user.save!
-
-      expect(user.password).not_to be_empty
-      expect(user.password_confirmation).not_to be_empty
+      
+      user2 = User.create(
+        first_name: "first2",
+        last_name: 'last2',
+        email: 'Email@email.com',
+        password: 'password2',
+        password_confirmation: 'password2'
+      )
+      expect(user).to be_valid
+      expect(user2).to_not be_valid
     end
 
-    
+    it "validates first_name last_name and email" do
+      user = User.create(
+        first_name: "",
+        last_name: 'last',
+        email: 'email@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      
+      user2 = User.create(
+        first_name: "first2",
+        last_name: '',
+        email: 'myemail@email.com',
+        password: 'password2',
+        password_confirmation: 'password2'
+      )
+
+      user3 = User.create(
+        first_name: "first3",
+        last_name: 'last3',
+        email: nil,
+        password: 'password3',
+        password_confirmation: 'password3'
+      )
+      expect(user).to_not be_valid
+      expect(user2).to_not be_valid
+      expect(user3).to_not be_valid
+    end
+
+    it "validate a min length of password" do
+      user = User.create(
+        first_name: "first",
+        last_name: 'last',
+        email: 'email@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      
+      user2 = User.create(
+        first_name: "first2",
+        last_name: 'last2',
+        email: 'myemail@email.com',
+        password: 'pa',
+        password_confirmation: 'pa'
+      )
+      expect(user).to be_valid
+      expect(user2).to_not be_valid
+    end
   end
 end
