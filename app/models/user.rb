771bdc795ba096b28bd96619aabc762be9454523
charleenmperrier <charlee_perrier@hotmail.com>
2021-04-06 01:42:12 +0000

    user validations uncommented

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :email, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, :uniqueness => { :case_sensitive => false }
   # validates :password_digest, presence: true
   
 
